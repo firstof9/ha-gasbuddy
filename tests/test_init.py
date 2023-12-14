07a308d407dd1a5b30cdbc785bd06b5574baddad
Chris <firstof9@gmail.com>
2023-12-14 08:16:01 -0700

    fix: options not saving (#18)

--- tests/test_init.py
+++ tests/test_init.py
@@ -5,7 +5,7 @@ from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.gasbuddy.const import DOMAIN
-from tests.const import CONFIG_DATA
+from tests.const import CONFIG_DATA, CONFIG_DATA_V1
 
 pytestmark = pytest.mark.asyncio
 
@@ -13,7 +13,31 @@ pytestmark = pytest.mark.asyncio
 async def test_setup_and_unload_entry(hass, mock_gasbuddy):
     """Test setup_entry."""
     entry = MockConfigEntry(
-        domain=DOMAIN, title="gas_station", data=CONFIG_DATA, version=1
+        domain=DOMAIN, title="gas_station", data=CONFIG_DATA, version=2
+    )
+
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 4
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
+
+    assert await hass.config_entries.async_unload(entries[0].entry_id)
+    await hass.async_block_till_done()
+    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 4
+    assert len(hass.states.async_entity_ids(DOMAIN)) == 0
+
+    assert await hass.config_entries.async_remove(entries[0].entry_id)
+    await hass.async_block_till_done()
+    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 0
+
+
+async def test_setup_and_unload_entry_v1(hass, mock_gasbuddy):
+    """Test setup_entry."""
+    entry = MockConfigEntry(
+        domain=DOMAIN, title="gas_station", data=CONFIG_DATA_V1, version=1
     )
 
     entry.add_to_hass(hass)
