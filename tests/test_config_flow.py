ac90154e6c1a9999608b113ff98b2f842bb89a22
firstof9@gmail.com <firstof9@gmail.com>
2024-12-12 16:16:41 -0700

    fix: insert 'fake' station when no stations found and provide error message

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -19,6 +19,8 @@ from custom_components.gasbuddy.const import (
 )
 from tests.const import CONFIG_DATA, STATION_LIST
 
+NO_STATIONS_LIST = { "-": "No stations in search area." }
+
 pytestmark = pytest.mark.asyncio
 
 
@@ -225,3 +227,128 @@ async def test_form_manual(
 
         await hass.async_block_till_done()
         assert len(mock_setup_entry.mock_calls) == 1
+
+
+@pytest.mark.parametrize(
+    "input,step_id,title,data",
+    [
+        (
+            {
+                CONF_NAME: DEFAULT_NAME,
+                CONF_STATION_ID: "208656",
+            },
+            "user",
+            DEFAULT_NAME,
+            {
+                CONF_NAME: DEFAULT_NAME,
+                CONF_STATION_ID: "208656",
+                CONF_INTERVAL: 3600,
+                CONF_UOM: True,
+            },
+        ),
+    ],
+)
+async def test_form_home_no_stations(
+    input,
+    step_id,
+    title,
+    data,
+    hass,
+    mock_gasbuddy,
+):
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.MENU
+    assert result["step_id"] == step_id
+
+    with patch(
+        "custom_components.gasbuddy.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry, patch(
+        "custom_components.gasbuddy.config_flow._get_station_list",
+        return_value=NO_STATIONS_LIST,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {"next_step_id": "search"}
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] == FlowResultType.MENU
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {"next_step_id": "home"}
+        )
+
+        assert result["type"] == FlowResultType.FORM
+        assert result["step_id"] == "home"
+        assert result["errors"] == {'station_id': 'no_results'}
+
+
+@pytest.mark.parametrize(
+    "input,input2,title,data",
+    [
+        (
+            {
+                CONF_POSTAL: "85396",
+            },
+            {
+                CONF_STATION_ID: "208656",
+                CONF_NAME: DEFAULT_NAME,
+            },
+            DEFAULT_NAME,
+            {
+                CONF_NAME: DEFAULT_NAME,
+                CONF_STATION_ID: "208656",
+                CONF_INTERVAL: 3600,
+                CONF_UOM: True,
+            },
+        ),
+    ],
+)
+async def test_form_postal_no_stations(
+    input,
+    input2,
+    title,
+    data,
+    hass,
+    mock_gasbuddy,
+):
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.MENU
+    assert result["step_id"] == "user"
+
+    with patch(
+        "custom_components.gasbuddy.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry, patch(
+        "custom_components.gasbuddy.config_flow._get_station_list",
+        return_value=NO_STATIONS_LIST,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {"next_step_id": "search"}
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] == FlowResultType.MENU
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {"next_step_id": "postal"}
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] == FlowResultType.FORM
+
+        assert result["step_id"] == "postal"
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], input
+        )
+
+        assert result["type"] == FlowResultType.FORM
+        assert result["step_id"] == "station_list"
+        assert result["errors"] == {'station_id': 'no_results'}
+   
